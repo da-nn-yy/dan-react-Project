5751952e641a34d0fd376d983ae8bc4d81aa0306
da-nn-yy <db7394928@gmail.com>
2024-02-29 16:39:14 -0900

    added: Containers and Component file by their folder name

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export {default as Articles} from './/Articles/Articles';
+export {default as Brand} from './/Brand/Brand';
+export {default as CTA} from './/CTA/Cta';
+export {default as Features} from './/Features/Features';
+export {default as Navbar} from './/Navbar/Navbar';
